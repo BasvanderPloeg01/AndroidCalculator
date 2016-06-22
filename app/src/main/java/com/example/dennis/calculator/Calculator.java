f7ea5b5e62d81cb3522ace7f2f7f186972871842
Dennis <dennisslimmers@live.nl>
2016-06-22 09:02:48 +0200

    Code optimisation

---
+++ app/src/main/java/com/example/dennis/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package com.example.dennis.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+public class Calculator extends AppCompatActivity {
+    public EditText e;
+
+    public Calculator(EditText et) {
+        this.e = et;
+    }
+}
--- app/src/main/java/com/example/dennis/calculator/MainActivity.java
+++ app/src/main/java/com/example/dennis/calculator/MainActivity.java
@@ -2,29 +2,12 @@ package com.example.dennis.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     EditText e;
-    Button one;
-    Button two;
-    Button three;
-    Button four;
-    Button five;
-    Button six;
-    Button seven;
-    Button eight;
-    Button nine;
-    Button nul;
-    Button plus;
-    Button minus;
-    Button times;
-    Button divided;
-    Button equals;
-    Button dot;
     String tempString = "";
     Float awnser = 0f;
     String[] splitString;
@@ -34,9 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       instanceObj();
-
-
+        e = (EditText) findViewById(R.id.editText);
     }
 
 
@@ -46,42 +27,44 @@ public class MainActivity extends AppCompatActivity {
         e.setText(tempString);
     }
 
-    public void calculate(View v)
-    {
-        switch (v.getId()){
+    public void calculate(View v) {
+        switch (v.getId()) {
             case R.id.button18:
                 if(tempString.contains("+")) {
                     splitString = tempString.split("\\+");
                      awnser = Float.parseFloat(splitString[0]) + Float.parseFloat(splitString[1]);
                     e.setText("" + awnser);
                 }
-                if(tempString.contains("-")){
+
+                if(tempString.contains("-")) {
                     splitString = tempString.split("\\-");
                      awnser = Float.parseFloat(splitString[0])-Float.parseFloat(splitString[1]);
                     e.setText(""+awnser);
                 }
-                if(tempString.contains("*")){
+
+                if(tempString.contains("*")) {
                     splitString = tempString.split("\\*");
                      awnser = Float.parseFloat(splitString[0])*Float.parseFloat(splitString[1]);
                     e.setText(""+awnser);
                 }
-                if(tempString.contains("/")){
+
+                if(tempString.contains("/")) {
                     splitString = tempString.split("/");
                      awnser = Float.parseFloat(splitString[0])/Float.parseFloat(splitString[1]);
                     e.setText(""+awnser);
                 }
+
                 break;
         }
     }
 
     public void addNumber(View v) {
         Button btn = (Button) v;
-        if(e.getText().equals("")||(awnser!=0f))
-        {
+        if(e.getText().equals("") || (awnser!=0f)) {
             e.setText(btn.getText());
             tempString = ""+e.getText();
             awnser = 0f;
-        }else{
+        } else {
             tempString = (""+e.getText()) + (""+btn.getText());
             e.setText(tempString);
         }
@@ -91,26 +74,6 @@ public class MainActivity extends AppCompatActivity {
     public void clearEntry(View v) {
         tempString = "";
         e.setText("");
-    }
-
-    public void instanceObj(){
-        e = (EditText) findViewById(R.id.editText);
-        one = (Button) findViewById(R.id.button14);
-        two  = (Button) findViewById(R.id.button15);
-        three  = (Button) findViewById(R.id.button16);
-        four  = (Button) findViewById(R.id.button11);
-        five  = (Button) findViewById(R.id.button12);
-        six  = (Button) findViewById(R.id.button13);
-        seven = (Button) findViewById(R.id.button8);
-        eight  = (Button) findViewById(R.id.button9);
-        nine  = (Button) findViewById(R.id.button10);
-        nul  = (Button) findViewById(R.id.button17);
-        plus = (Button) findViewById(R.id.button4);
-        minus = (Button) findViewById(R.id.button5);
-        times = (Button) findViewById(R.id.button6);
-        divided = (Button) findViewById(R.id.button7);
-        equals = (Button) findViewById(R.id.button18);
-        dot = (Button) findViewById(R.id.button19);
     }
 }
 
